7cf28b1df7f49a5370fe0d6caa4e2bd11021951e
master agod <agodsouruintan3nd@gmail.com>
2019-07-29 02:01:44 +0900

    簡単な問題の復習

---
+++ joytas/Day23top.java
@@ -0,0 +1,12 @@
+import my.util.*;
+public class Day23top{
+	public static void main(String[] args){
+		int x=23;
+		int y=6;
+		Common.print("x+y="+(x+y));
+		Common.print("x-y="+(x-y));
+		Common.print("x*y="+(x*y));
+		Common.print("x/y="+(x/y));
+		Common.print("x%y="+(x%y));
+	}
+}
