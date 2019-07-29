a76de7fcfce779c56b740e054001720b0478bcd8
master agod <agodsouruintan3nd@gmail.com>
2019-07-29 14:45:15 +0900

    継承について

---
+++ day26/Hero2.java
@@ -0,0 +1,8 @@
+import my.util.*;
+public class Hero2{
+	private String name="ミナト";
+	private int hp=100;
+	public void run(){
+		Common.print(this.name+"は逃げ出した！");
+	}
+}
---
+++ day26/SuperHero.java
@@ -0,0 +1,15 @@
+import my.util.*;
+public class SuperHero extends Hero2{
+	private boolean flying;
+	public void fly(){
+		this.flying=true;
+		Common.print("飛び上がった！");
+	}
+	public void land(){
+		this.flying=false;
+		Common.print("着地した！");
+	}
+	public void run(){
+		Common.print("撤退した！");
+	}
+}
