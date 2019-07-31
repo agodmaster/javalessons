7f9723ccb0444f83554c3a58c0963c6b7954457c
master agod <agodsouruintan3nd@gmail.com>
2019-07-31 11:03:29 +0900

    attackメソッドをオーバライドして二回攻撃を追加した

--- rpg2/Hero.java
+++ rpg2/Hero.java
@@ -2,11 +2,20 @@ public class Hero{
 	private String name="ミナト";
 	private int hp=100;
 	public void attack(Matango m){
-		System.out.println(this.name+"の攻撃！");
+		System.out.println(this.getName()+"の攻撃！");
 		m.hp -=5;
 		System.out.println("5ポイントのダメージをあたえた");
 	}
+	public final void slip(){
+		this.hp -=5;
+		System.out.println(this.getName()+"は転んだ！");
+		System.out.println("5のダメージ");
+
+	}
 	public void run(){
-		System.out.println(this.name+"は逃げ出した！");
+		System.out.println(this.getName()+"は逃げ出した！");
+	}
+	public String getName(){
+		return this.name;
 	}
 }
