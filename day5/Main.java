cfcef093c504abd30980d3a72c18fce113a870f4
master agod <agodsouruintan3nd@gmail.com>
2019-08-17 22:17:33 +0900

    ifの使い方20含む以上の年齢は大人ですというもの

---
+++ day5/Main.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static void main(String[] args){
+        int age=32;
+		if(age>=20){
+			System.out.println("大人です");
+		}
+		System.out.println("処理を終わります");
+    }
+}
