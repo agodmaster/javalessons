d26262e10f5d482eb4ac1bf1e298a75723789a56
master agod <agodsouruintan3nd@gmail.com>
2019-08-17 23:14:44 +0900

    光の三原色RGB(red,green,blue)か判定するアプリ

---
+++ day5/Main9.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main9{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("色を入力>");
+        String color=sc.nextLine();
+        if(color.equals("赤") || color.equals("緑") || color.equals("青")){
+            System.out.println(color+"は光の三原色です。");
+        }else{
+            System.out.println(color+"は光の三原色ではありません。");
+        }
+    }
+}
