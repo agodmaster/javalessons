146e1b71a2975a26039c0d9a0c5197671508271e
master agod <agodsouruintan3nd@gmail.com>
2019-08-17 23:48:00 +0900

    正の数字であるのと偶数である時のネスト

---
+++ day5/Main13.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main13{
+    public static void main(String[] args){
+        int num=21;
+        if(num >0){
+            if(num%2==0){
+                System.out.println(num+"は正の偶数です");
+            }
+        }
+    }
+}
