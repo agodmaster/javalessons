edcad15915ad4076dd134a4b8b26c57d6530f610
master agod <agodsouruintan3nd@gmail.com>
2019-08-17 15:18:32 +0900

    細かいところの確認と編集

--- day3/Main.java
+++ day3/Main.java
@@ -1,16 +1,16 @@
 public class Main{
     public static void main(String[] args){ 
-    char name='小';
-    char name1='林';
-    char name2='拓';
-    char name3='也';
+    char name='苗';
+    char name1='字';
+    char name2='名';
+    char name3='前';
     int name4=name+name1+name2+name3;
     int ave=name4/4;
     System.out.println("ポイントは"+name4+"平均は"+ave); 
-    System.out.println("小"+(int)name);
-    System.out.println("林"+(int)name1);
-    System.out.println("拓"+(int)name2);
-    System.out.println("也"+(int)name3);
+    System.out.println("苗"+(int)name);
+    System.out.println("字"+(int)name1);
+    System.out.println("名"+(int)name2);
+    System.out.println("前"+(int)name3);
     int max=Math.max(Math.max(name,name1),Math.max(name2,name3));
     System.out.println("最強文字は:"+max+"powerです");
 }
--- der1/Main5.java
+++
@@ -1,8 +0,0 @@
-public class Main5{
- public static void main(String[]args){
-int appleprice=120;
-int num=5;//個数
-int total=appleprice*num;
-System.out.println("今日のお買い物は"+total+"円です");
- }
-}
