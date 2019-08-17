29b280a01aeaca2ce390ad34ebbdd3cc90ab2a60
master agod <agodsouruintan3nd@gmail.com>
2019-08-17 16:05:23 +0900

    商の求め方

---
+++ day2/Main5.java
@@ -0,0 +1,8 @@
+public class Main5{
+    public static void main(String[] args){
+        int num1=10;
+        int num2=3;
+        System.out.println("num1:"+num1+"をnum2:"+num2+"で割った商は"+(num1/num2)+"余りは"+(num1%num2)+"です。");
+    }
+
+}
