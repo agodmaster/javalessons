18ec1fd59c7cc1fa89c6215eb28aabe8e473230f
master agod <agodsouruintan3nd@gmail.com>
2019-08-17 17:08:22 +0900

    計算式での省略について

---
+++ day3/Main5.java
@@ -0,0 +1,17 @@
+public class Main5{
+	public static void main(String[] args){
+        int x=10;
+		//x=x+1とx+=1は同じ意味
+        //x=x+1;
+        //x+=1;
+        x=x*2;
+        x*=2;
+        x=x/3;
+        x/=3;
+        x=x%2;
+        x%=2;
+        x=x-4;
+        x-=4;
+        System.out.println(x);
+    }
+}
