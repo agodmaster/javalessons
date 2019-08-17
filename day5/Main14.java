53fd54502fb0216d0f566677afe7aa7f5fb30ce1
master agod <agodsouruintan3nd@gmail.com>
2019-08-18 00:01:53 +0900

    if-else文には必ずどちらかを通る保証がある構文について

---
+++ day5/Main14.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main14{
+    public static void main(String[] args){
+        int num=21;
+        String result;
+        if(num ==21){
+            result="BlackJack!";
+        }
+        System.out.println(result);//初期化されてないのを使おうとしてる
+    }
+}
---
+++ day5/Main14ver2.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main14ver2{
+    public static void main(String[] args){
+        int num=21;
+        String result;
+        if(num ==21){
+            result="BlackJack!";
+        }else{
+            result="No BlackJack";
+        }
+        System.out.println(result);//if-else文は必ず通る保証がある
+    }
+}
