8eb9eec0d00281a4a5885fbf3c5d1d835d1a40b8
master agod <agodsouruintan3nd@gmail.com>
2019-08-17 17:22:39 +0900

    int同士をdoubleにするキャストについて

---
+++ day3/Main7.java
@@ -0,0 +1,8 @@
+public class Main7{
+    public static void main(String[] args){
+        int a=10;
+        int b=3;
+        double result=(double)a/b;//int型同士で割り算する際は片方をキャスト
+        System.out.println(result);
+    }
+}
