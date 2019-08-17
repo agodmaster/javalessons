e8b81aa2fe918cf23681255922066ca4c42cfc18
master agod <agodsouruintan3nd@gmail.com>
2019-08-17 18:44:32 +0900

    day4のMain6の別のやり方

---
+++ day4/Main6ver2.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Main6ver2{
+    public static void main(String[] args){
+        Random rand=new Random();
+        int r=rand.nextInt(90);
+        System.out.println("あなたはたぶん、"+r+"歳ですね");
+    }
+}
