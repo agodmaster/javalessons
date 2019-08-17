8d0419460fa630af8bd6f5771cdf210033958991
master agod <agodsouruintan3nd@gmail.com>
2019-08-17 18:25:52 +0900

    String文字列をint数字に変換する命令のやり方

---
+++ day4/Main5.java
@@ -0,0 +1,7 @@
+public class Main5{
+    public static void main(String[] args){
+        String age="20";
+        int ageInt=Integer.parseInt(age);
+        System.out.println("来年は"+(ageInt+1)+"歳ですね");
+    }
+}
