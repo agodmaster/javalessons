50e14b3f568b0a0a5a84f3836fe2883341256e24
master agod <agodsouruintan3nd@gmail.com>
2019-08-17 22:22:46 +0900

    フォルダ名変更の為削除

--- dar5/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main{
-    public static void main(String[] args){
-        int num=2;
-        if(num != 0){
-            System.out.println("numは0ではありません");
-        }
-        System.out.println("処理終了");
-    }
-}
