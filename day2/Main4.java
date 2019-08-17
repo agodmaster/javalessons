73b67025d0e75b181e41c24d419075c7190a4bd2
master agod <agodsouruintan3nd@gmail.com>
2019-08-17 16:01:10 +0900

    三角形の面積の求め方

---
+++ day2/Main4.java
@@ -0,0 +1,9 @@
+public class Main4{
+    public static void main(String[] args){
+        double bottom=3.5;
+        double height=4.2;
+        double area=bottom*height/2;
+        System.out.println("底辺が"+bottom+",高さが"+height+"の三角形の面積は"+area+"です。");
+    }
+
+}
