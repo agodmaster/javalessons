f7cc38b515b0608b7091a2012fe3c3fc72c6f1ae
master agod <agodsouruintan3nd@gmail.com>
2019-08-17 22:36:35 +0900

    数値が０でない時if文が動く処理

---
+++ day5/Main3.java
@@ -0,0 +1,9 @@
+public class Main3{
+    public static void main(String[] args){
+        int num=2;
+        if(num != 0){
+            System.out.println("numは0ではありません");
+        }
+        System.out.println("処理終了");
+    }
+}
