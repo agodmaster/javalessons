27cdf769ed7590923fbc55485d960e3ba0e5e467
master agod <agodsouruintan3nd@gmail.com>
2019-08-17 18:17:51 +0900

    String型文字列の数値も文字列になるよ

---
+++ day4/Main4.java
@@ -0,0 +1,7 @@
+public class Main4{
+    public static void main(String[] args){
+        String age="20";
+        System.out.println("来年は"+(age+1)+"歳ですね");
+		//String型は数値でも文字列として扱うので結果201になる
+    }
+}
