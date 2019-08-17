97b762200ca78eb94c5ce028f16fd71afcf33180
master agod <agodsouruintan3nd@gmail.com>
2019-08-17 18:37:43 +0900

    乱数を生成する命令について

---
+++ day4/Main6.java
@@ -0,0 +1,7 @@
+public class Main6{
+    public static void main(String[] args){
+		int r=new java.util.Random().nextInt(90);
+		//0含む89歳までの乱数90個
+		System.out.println("あなたはたぶん、"+r+"歳ですね");
+    }
+}
