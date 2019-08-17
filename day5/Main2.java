d787bd721cf0118256d9f0b5d74d509fe0ccf743
master agod <agodsouruintan3nd@gmail.com>
2019-08-17 22:28:59 +0900

    群数の時に群数ですと出力する

---
+++ day5/Main2.java
@@ -0,0 +1,9 @@
+public class Main2{
+    public static void main(String[] args){
+        int age=16;
+        if(age % 2 == 0){
+            System.out.println("偶数です");
+        }
+        System.out.println("処理を終わります");
+    }
+}
