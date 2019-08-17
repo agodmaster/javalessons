1f097b8b29300132177e420fde438cbf58901700
master agod <agodsouruintan3nd@gmail.com>
2019-08-17 19:06:23 +0900

    キーボードから受け付ける命令について2パターン作成

---
+++ day4/Main7ver2.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main7ver2{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("あなたの名前を入力してください。");
+        String name=sc.nextLine();
+        System.out.println("あなたの年齢を入力してください。");
+        int age=sc.nextInt();
+        System.out.println("ようこそ、"+age+"歳の"+name+"さん");
+
+    }
+}
