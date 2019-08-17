8c8c184b22d6c4422b6f56670152b178cef6e2db
master agod <agodsouruintan3nd@gmail.com>
2019-08-17 12:31:53 +0900

    一部訂正

--- der1/Main5.java
+++ der1/Main5.java
@@ -1,7 +1,7 @@
 public class Main5{
  public static void main(String[]args){
 int appleprice=120;
-int num=5;
+int num=5;//個数
 int total=appleprice*num;
 System.out.println("今日のお買い物は"+total+"円です");
  }
