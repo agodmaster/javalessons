042ec9d6b7114b360137c88ff93ac540d8107a38
master agod <agodsouruintan3nd@gmail.com>
2019-08-17 18:08:50 +0900

    Math.max(大きい方の数字の代入)

---
+++ day4/Main3.java
@@ -0,0 +1,8 @@
+public class Main3{
+    public static void main(String[] args){
+        int a=10;
+        int b=20;
+        int max=Math.max(a,b);
+        System.out.println(a+"と"+b+"大きい方は"+max+"です");
+    }
+}
