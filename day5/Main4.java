e71e7cba752e228dadaddd28def0a70efa959b8c
master agod <agodsouruintan3nd@gmail.com>
2019-08-17 22:41:03 +0900

    20より上なら大人そうでない時子どもと出力される処理

---
+++ day5/Main4.java
@@ -0,0 +1,10 @@
+public class Main4{
+    public static void main(String[] args){
+        int age=12;
+        if(age>=20){
+            System.out.println("大人です");
+        }else{
+            System.out.println("子どもです");
+        }
+    }
+}
