630dd49dd831e3ababb3afe634200a268de18533
master agod <agodsouruintan3nd@gmail.com>
2019-08-18 16:46:08 +0900

    温度を一度ずつ指定の値まで下げる処理をwhile文で作成

---
+++ day6/Main5.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main5{
+    public static void main(String[] args){
+        int temp=30;
+        while(temp>22){
+            temp--;
+            System.out.println("温度を一度下げました。現在室温"+temp+"度");
+        }
+    }
+}
