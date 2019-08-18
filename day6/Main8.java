1dd9e8b2dd333d11f77e9a153c712c27da6e0cd2
master agod <agodsouruintan3nd@gmail.com>
2019-08-18 17:14:45 +0900

    カウントダウンロケットの作成

---
+++ day6/Main8.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main8{
+    public static void main(String[] args){
+        int n=9;
+        while(n>0){
+            System.out.println(n);
+            n--;
+        }
+        System.out.println("発射!");
+    }
+}
