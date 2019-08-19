2bd06855f907286a4ef1b5875f69a8b1ae98f41d
master agod <agodsouruintan3nd@gmail.com>
2019-08-19 13:57:00 +0900

    入力した整数範囲を出力するクリア

---
+++ homework/Lesson005.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Lesson005{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		System.out.print("整数を入力＞");
+		int num=sc.nextInt();
+		for(int i=1;i<=num;i++){
+			System.out.print(i+" ");
+		}
+		System.out.println();
+	}
+}
