14fa69a6be431a9fc098c7be3674ba569f37195e
master agod <agodsouruintan3nd@gmail.com>
2019-08-20 08:51:49 +0900

    ２つの整数と偶数と奇数を選び、その間の合計を表示する＋大小逆入力や負の数にも対応するクリア

---
+++ homework/Lesson010.java
@@ -0,0 +1,43 @@
+import java.util.*;
+public class Lesson010{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		System.out.print("整数１を入力。");
+		int num=sc.nextInt();
+		System.out.print("整数２を入力＞");
+		int num2=sc.nextInt();
+		System.out.print("偶数(1)・奇数(2)を選択＞");
+		int select=sc.nextInt();
+		int max=0;
+		int min=0;
+		int sum=0;
+		if(num <= num2){
+			max = num2;
+			min = num;
+		}else{
+			max = num;
+			min = num2;
+		}
+		switch(select){
+		case 1:
+			for(int i = max ; i > min ; i--){
+				if(i %2 == 0){
+					sum+=i;
+				}
+			}
+			System.out.print(min+"から"+max+"までの偶数の合計は"+sum+"です。 ");
+			break;
+		case 2:
+			for(int i = max ; i > min ; i--){
+				if(i %1 == 0){
+					sum+=i;
+				}
+			}
+			System.out.print(min+"から"+max+"までの偶数の合計は"+sum+"です。 ");
+			break;
+		default:
+			System.out.print("アプリを終了します");
+			return;
+		}
+	}
+}
