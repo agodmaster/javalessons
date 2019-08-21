1737cb5eb5a7588981eb953404f21fef049c840d
master agod <agodsouruintan3nd@gmail.com>
2019-08-21 09:19:24 +0900

    コメントの追加

--- homework/Lesson010.java
+++ homework/Lesson010.java
@@ -6,30 +6,30 @@ public class Lesson010{
 		int num=sc.nextInt();
 		System.out.print("整数２を入力＞");
 		int num2=sc.nextInt();
-		System.out.print("偶数(1)・奇数(2)を選択＞");
-		int select=sc.nextInt();
-		int max=0;
-		int min=0;
-		int sum=0;
-		if(num <= num2){
+		System.out.print("偶数(1)・奇数(2)を選択＞");//分岐選択
+		int select=sc.nextInt();//分岐用整数
+		int max=0;//maxの値代入用
+		int min=0;//min同上
+		int sum=0;//合計を入れる箱
+		if(num <= num2){//大小の入れ替え
 			max = num2;
 			min = num;
 		}else{
 			max = num;
 			min = num2;
 		}
-		switch(select){
-		case 1:
+		switch(select){//分岐
+		case 1://偶数の合計
 			for(int i = max ; i >= min ; i--){
-				if(i %2 == 0){
+				if(i %2 == 0){//2で割り切れる時
 					sum+=i;
 				}
 			}
 			System.out.print(min+"から"+max+"までの偶数の合計は"+sum+"です。 ");
 			break;
-		case 2:
+		case 2://奇数の合計
 			for(int i = max ; i > min ; i--){
-				if(i %2 != 0){
+				if(i %2 != 0){//2で割り切れない時
 					sum+=i;
 				}
 			}
